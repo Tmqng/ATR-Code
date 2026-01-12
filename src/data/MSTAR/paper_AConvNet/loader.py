8d48bbdff80451944528e28988f16d67f3438b71
Tmqng <minhquant0911@gmail.com>
2026-01-12 11:37:28 +0100

    debug

--- src/data/MSTAR/paper_AConvNet/loader.py
+++ src/data/MSTAR/paper_AConvNet/loader.py
@@ -55,7 +55,8 @@ class Dataset(torch.utils.data.Dataset):
             image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # dim 2 if it's png
             if len(image.shape) < 3:
                 image = np.expand_dims(image, axis=0) # add channel dim
-            self.images.append(image)
+            image_tensor = torch.from_numpy(image).float()
+            self.images.append(image_tensor)
             # self.images.append(np.load(image_path))
 
             with open(label_path, mode='r', encoding='utf-8') as f:
