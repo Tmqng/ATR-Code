e6b26ae48addb2acf849167827ea37432e818499
dacher <mael.dacher@student.isae-supaero.fr>
2026-01-26 17:13:19 +0100

    Added target names for 'all' mstar config

--- src/data/MSTAR/Felipe/process_mstar_AConvNet.py
+++ src/data/MSTAR/Felipe/process_mstar_AConvNet.py
@@ -152,6 +152,8 @@ target_name_eoc_2 = ('BMP2', 'BRDM2', 'BTR70', 'T72')
 
 target_name_confuser_rejection = ('BMP2', 'BTR70', 'T72', '2S1', 'ZIL131')
 
+target_name_all = set(target_name_soc + target_name_eoc_1 + target_name_eoc_2 + target_name_confuser_rejection)
+
 # serial_number = {
 #     # 2S1
 #     'b01': 0, 
